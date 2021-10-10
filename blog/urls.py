a3e51c14b14d852ff2269b6e3ffa0a6bf2a4e163
ANURAG VERMA <20cse08@ietagra.org.in>
2021-10-10 16:59:40 +0530

    added all post and view it according to categories

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('index', views.index),
+    path('about', views.about),
+    path('contact', views.contact),
+    path('post/<slug:url>', views.post),
 
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,39 @@
 from django.shortcuts import render
-
+from blog.models import Category, Post
 # Create your views here.
 
 
-def home(request):
-    return render(request, 'index.html')
+def index(request):
+    posts = Post.objects.all()
+    cats = Category.objects.all()
+    data = {
+        'posts': posts,
+        'cats': cats,
+    }
+    return render(request, 'index.html', data)
+
+
+def post(request, url):
+    post = Post.objects.get(url=url)
+    cats = Category.objects.all()
+    data = {
+        'post': post,
+        'cats': cats,
+    }
+    return render(request, 'post.html', data)
+
+
+def about(request):
+    cats = Category.objects.all()
+    data = {
+        'cats': cats,
+    }
+    return render(request, 'about.html', data)
+
+
+def contact(request):
+    cats = Category.objects.all()
+    data = {
+        'cats': cats,
+    }
+    return render(request, 'contact.html', data)
