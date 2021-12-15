883b84d6523cd9de4f18a3cd20818dd4b8d8e4a6
anurag629 <akvermaav629@gmail.com>
2021-12-15 23:06:29 +0530

    completed all collagepaper

--- azblog/settings.py
+++ azblog/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sncdj4dj-@4wo5+t_$1^_8s2_cx8m%xiof^(@wok*(ybvv%7%*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
